877405abf04dfac5bf0a030ed4e055f735d93053
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-04-23 19:56:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const hat_top = document.querySelector('#hat_top');
 const hat_bottom = document.querySelector('#hat_bottom');
 
-//hat_top.addEventListener('mouseover', rise);
+hat_top.addEventListener('mouseover', rise);
 
-function rise(){
+    function rise(){
+
         hat_top.style.margin = '-60px 0px';
         hat_top.style.transform = 'rotate(30deg)';
 
         hat_bottom.style.margin = '-40px -15px';
         hat_bottom.style.transform = 'rotate(-60deg)';
-}
+
+    }
 
 
-setInterval(rise, 1000)
+
+//setInterval(rise, 1000)
